690220ce8831b5679dd249add1753ffb8a411b21
memechanic <mikhailvost228@gmail.com>
2024-06-07 09:54:25 +0700

    Added first version of programm
    trying my best

---
+++ MP3Player/Program.cs
@@ -0,0 +1,12 @@
+﻿using ClassLibrary;
+
+namespace MP3Player
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
